1f0b6a591710935d3391115d2d66a7e812047eb5
MMane <mmane@dept-info.crosemont.quebec>
2023-12-20 15:28:15 -0500

    Affichage des pages accueil, services, contact et des chambres

--- ProjetHotel/Controllers/HomeController.cs
+++ ProjetHotel/Controllers/HomeController.cs
@@ -28,5 +28,15 @@ namespace ProjetHotel.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        public IActionResult Services()
+        {
+            return View();
+        }
     }
 }
